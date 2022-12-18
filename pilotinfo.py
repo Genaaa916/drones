35be276c08d20da9aa1af3257d5f97ca95c24bb0
Gena <genci.behxheti@gmail.com>
2022-12-18 22:48:54 +0200

    refactor: move pilotinfo to its own file

---
+++ pilotinfo.py
@@ -0,0 +1,15 @@
+class pilot:
+    def __init__(self) -> None:
+        pass
+
+    def lisaa_nro(nro):
+        pilot.number = nro
+
+    def lisaa_x(x):
+        pilot.x = x
+
+    def lisaa_y(y):
+        pilot.y = y
+
+    def print():
+        print(pilot.number, pilot.x, pilot.y)
