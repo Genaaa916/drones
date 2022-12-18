f29ddaff4e3ae5158cfdc14c3398bc57cf58dff6
Gena <genci.behxheti@gmail.com>
2022-12-18 22:42:51 +0200

    pull drone position data

---
+++ req.py
@@ -0,0 +1,61 @@
+import requests
+import bs4 as bs
+import lxml.etree as le
+import time
+
+
+class pilot:
+    def __init__(self) -> None:
+        pass
+
+    def lisaa_nro(nro):
+        pilot.number = nro
+
+    def lisaa_x(x):
+        pilot.x = x
+
+    def lisaa_y(y):
+        pilot.y = y
+
+    def print():
+        print(pilot.number, pilot.x, pilot.y)
+
+
+url = "https://assignments.reaktor.com/birdnest/drones"
+
+
+def get_data(url):
+    r = requests.get(url)
+    r = r.text
+    result = bs.BeautifulSoup(r, 'lxml')
+    soupy = result.find_all("positiony")
+    soupx = result.find_all("positionx")
+    result_y = []
+    result_x = []
+    for i in soupy:
+        result_y.append(i.text)
+    for i in soupx:
+        result_x.append(i.text)
+    result = result_y + result_x
+
+    print(result)
+
+    number = 1
+    pilotlist = []
+    for i in range(len(result_x)):
+        pilot_ = pilot
+        pilot.lisaa_nro(number)
+        number += 1
+        pilot.lisaa_x(float(result_x[i]))
+        pilot.lisaa_y(float(result_y[i]))
+        pilot.print()
+        if pilot_.y in range(150000, 350000) and pilot_.x in range(150000, 350000):
+            print(f"Pilot {pilot.number} is in the zone!")
+        else:
+            print(f"Pilot {pilot.number} is not in the zone!")
+        pilotlist.append(pilot_)
+        time.sleep(5)
+    get_data(url)
+
+
+get_data(url)
